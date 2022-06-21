a89a55249af4fd226b31ded8e80df95cf2f0584d
whenipush <sunsetbb@yahoo.com>
2022-06-21 13:24:24 +0300

    QuerySet lesson

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Order
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+
+    return render(request, './index.html', { 'object_list': object_list })
