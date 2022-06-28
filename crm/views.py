3c991cc930014aa1f5caa29ff1d7b1de408fb071
whenipush <sunsetbb@yahoo.com>
2022-06-28 12:03:59 +0300

    form handlers

--- crm/views.py
+++ crm/views.py
@@ -5,3 +5,11 @@ def first_page(request):
     object_list = Order.objects.all()
 
     return render(request, './index.html', { 'object_list': object_list })
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name = name, order_phone = phone)
+    element.save()
+    return render(request, './thanks_page.html', {'name': name,
+                                                  'phone': phone})
--- landingpagewebsite/urls.py
+++ landingpagewebsite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from crm import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first_page)
+    path('', views.first_page),
+    path('thanks', views.thanks_page, name='thanks_page'),
 ]
