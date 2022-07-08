a70a453833d31b9cbdf3b412a96b82ce9eb39c57
whenipush <sunsetbb@yahoo.com>
2022-07-08 12:00:38 +0300

    template replacement

--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "landingpagewebsite/static/",
+]
+
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
